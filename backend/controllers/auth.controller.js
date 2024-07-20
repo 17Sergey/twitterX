09895664a6be10ff10d38aa7b68a2c472b45ee37
Sergey <sergeykachaliukdev@gmail.com>
2024-07-20 09:17:40 +0300

    1.1 - db model + signup controller + token&cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,59 @@
+import { generateTokenAndSetCookie } from "../lib/utils/generateTokenAndSetCookie.js";
+import User from "../models/user.model.js";
+import brypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-  res.json({
-    data: "Signup endpoint",
-  });
+  try {
+    const { username, fullName, email, password } = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      res.status(400).json({ error: "Invalid email format" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      res.status(400).json({ error: "User email is already taken" });
+    } 
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      res.status(400).json({ error: "Username is already taken" });
+    } 
+
+    const salt = await brypt.genSalt(10);
+    const hashedPassword = brypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+      });
+    }
+    else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+
+  } catch (error) {
+    console.log(`Error in signup controller ${error}`);
+    res.status(500); // Server error
+  }
 };
 
 export const login = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 5000;
